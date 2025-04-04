8cd62368214e274b180dae273b1b149573f4d476
zsheng <zsheng.dev@gmail.com>
2025-04-04 00:22:57 -0400

    fix: handle itunes url

--- components/new-podcast-card.tsx
+++ components/new-podcast-card.tsx
@@ -7,6 +7,8 @@ interface NewPodcastCardProps {
 }
 
 export function NewPodcastCard({ podcast, onAdd }: NewPodcastCardProps) {
+  const imageUrl = podcast.podcast.image?.url || podcast.podcast.itunesImage;
+
   return (
     <YStack gap='$3' alignItems='center' paddingBottom='$4' paddingTop='$2'>
       <YStack gap='$1' alignItems='center'>
@@ -18,9 +20,9 @@ export function NewPodcastCard({ podcast, onAdd }: NewPodcastCardProps) {
         </Text>
       </YStack>
 
-      {podcast.podcast.image?.url && (
+      {imageUrl && (
         <Image
-          source={{ uri: podcast.podcast.image.url }}
+          source={{ uri: imageUrl }}
           width={'75%'}
           aspectRatio={1}
           borderRadius={20}
@@ -29,7 +31,7 @@ export function NewPodcastCard({ podcast, onAdd }: NewPodcastCardProps) {
 
       <Button
         onPress={onAdd}
-        backgroundColor='rgba(211, 211, 211, 0.2)'
+        // backgroundColor='rgba(211, 211, 211, 0.2)'
         color='black'
         size='$5'
         width={'85%'}
