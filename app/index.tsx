8b3261983a3ff1e55acaf4ec2137b1bcc7464dd2
zsheng <zsheng.dev@gmail.com>
2025-04-04 00:36:17 -0400

    refactor: remove unnecessary buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,15 @@
 import { NewPodcastSheet } from '@/components/new-podcast-sheet';
+import { podcastsTable } from '@/db/schema';
+import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { ActivityIndicator } from 'react-native';
 import { Button, H2, Paragraph, ScrollView, YStack } from 'tamagui';
 import { PodcastList } from '../components/podcast-list';
 import db from '../db';
-import { podcastsTable } from '../db/schema';
 import migrations from '../drizzle/migrations';
-import { parsePodcastFeed } from '../lib/parser';
+import { PodcastFeed } from '../lib/parser';
 import { createPodcast, getPodcasts } from '../service/podcast';
-
-const PODCAST_RSS_FEED_URL = 'https://feeds.simplecast.com/82FI35Px';
 
 export default function Index() {
   const { success, error: migrationError } = useMigrations(db, migrations);
@@ -19,17 +18,7 @@ export default function Index() {
   const [error, setError] = useState<string | null>(null);
   const [saveStatus, setSaveStatus] = useState<string | null>(null);
 
-  const [podcasts, setPodcasts] = useState<
-    (typeof podcastsTable.$inferSelect)[] | null
-  >(null);
-
-  useEffect(() => {
-    if (!success) return;
-    (async () => {
-      const podcastList = await getPodcasts();
-      setPodcasts(podcastList);
-    })();
-  }, [success]);
+  const { data: podcasts } = useLiveQuery(db.select().from(podcastsTable));
 
   const [podcastSheetOpen, setPodcastSheetOpen] = useState(false);
 
@@ -40,30 +29,19 @@ export default function Index() {
     return <Paragraph>Migration error: {migrationError.message}</Paragraph>;
   }
 
-  async function refreshPodcasts() {
-    const podcastList = await getPodcasts();
-    setPodcasts(podcastList);
-  }
-
-  const savePodcast = async () => {
+  const savePodcast = async (podcastFeed: PodcastFeed) => {
     try {
-      setLoading(true);
-      setError(null);
-      setSaveStatus(null);
-
-      const feedData = await parsePodcastFeed(PODCAST_RSS_FEED_URL);
-
       // Check if podcast already exists
       const existingPodcasts = await getPodcasts();
       const exists = existingPodcasts.some(
-        (p) => p.title === feedData.podcast.title
+        (p) => p.title === podcastFeed.podcast.title
       );
 
       if (exists) {
-        setSaveStatus(`${feedData.podcast.title} already exists`);
+        setSaveStatus(`${podcastFeed.podcast.title} already exists`);
       } else {
-        await createPodcast(feedData);
-        setSaveStatus(`${feedData.podcast.title} saved!`);
+        await createPodcast(podcastFeed);
+        setSaveStatus(`${podcastFeed.podcast.title} saved!`);
       }
     } catch (err: unknown) {
       const errorMessage =
@@ -78,19 +56,11 @@ export default function Index() {
   return (
     <ScrollView>
       <YStack gap='$4' padding='$4' paddingVertical={'$6'}>
-        <H2 fontWeight={'bold'}>Podcasts</H2>
+        <Button onPress={() => setPodcastSheetOpen(true)} size='$5'>
+          Add Podcast
+        </Button>
 
-        <YStack gap='$3'>
-          <Button onPress={savePodcast} disabled={loading} size='$5'>
-            Save Podcast
-          </Button>
-          <Button onPress={refreshPodcasts} size='$5'>
-            Refresh Podcasts
-          </Button>
-          <Button onPress={() => setPodcastSheetOpen(true)} size='$5'>
-            Add Podcast
-          </Button>
-        </YStack>
+        <H2 fontWeight={'bold'}>Podcasts</H2>
 
         {loading && <ActivityIndicator />}
 
@@ -102,9 +72,11 @@ export default function Index() {
 
         {podcasts && <PodcastList podcasts={podcasts} />}
       </YStack>
+
       <NewPodcastSheet
         open={podcastSheetOpen}
         onOpenChange={setPodcastSheetOpen}
+        onAddPodcast={savePodcast}
       />
     </ScrollView>
   );
--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/expo-sqlite';
 import { openDatabaseSync } from 'expo-sqlite';
 import * as schema from './schema';
 
-const expo = openDatabaseSync('nod.db');
+const expo = openDatabaseSync('nod.db', { enableChangeListener: true });
 const db = drizzle(expo, { schema });
 
 export default db;
